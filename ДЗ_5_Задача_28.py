8cfc1ec4ec6be32655ed8ec475d540ea1335dc8a
Alexandr <dajj2012@mail.ru>
2023-03-09 20:55:19 +0300

    ДЗ после 5го занятия готовое

--- ДЗ_5_Задача_28.py
+++ ДЗ_5_Задача_28.py
@@ -3,6 +3,7 @@ def summ(a, b):
         summ(a+1, b-1)
     if b == 0:
         print(f'Сумма чисел равна {a}')
+        # return a
 
 numberB = int(input('Введите  число '))
 numberA = int(input('Введите  число '))
